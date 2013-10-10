4d125ff17dfed097a357de170e63746a99ba9a99
sergebb <thereisalogin@gmail.com>
2013-10-10 19:15:09 +0400

    Optimised colors and speed pca

--- rt_pca.py
+++ rt_pca.py
@@ -17,65 +17,7 @@ def pca(data):
 	(u,s,v) = np.linalg.svd(data, full_matrices=False)
 	return np.dot(data, v.T)
 
-def main():
-
-	df = open(DEAL,"r")
-	of = open(ORDER,"r")
-
-	order_time = []
-	ord_values = []
-	deals = None
-	line_num=0
-	color = []
-	data_price =[]
-	for line in df:
-		line_num+=1
-		color.append(1)
-		if line_num> N:
-			break
-		if line[0] == '#':
-			continue
-		date = line.split(',')[2]
-		price = int(line.split(',')[4].split('.')[0])
-		val = int(line.split(',')[5])
-		deal_type = line.split(',')[7]
-		# print date, "\t", price, "\t", val, "\t", deal_type,
-		deal_time = datetime.strptime(date, "%Y%m%d%H%M%S%f")
-		while True:
-			ordline = of.readline()
-			if ordline[0] == '#':
-				continue
-			ord_time = datetime.strptime(ordline.split(',')[3], "%Y%m%d%H%M%S%f")
-			order_time.append(ord_time)
-			ord_values.append(int(ordline.split(',')[7]))
-			if(ord_time>deal_time):
-				break
-
-		# time1 = datetime.timedelta([days[, seconds[, microseconds[, milliseconds[, minutes[, hours[, weeks]]]]]]])
-		time1 = timedelta(seconds=1)
-		time2 = timedelta(seconds=10)
-		time3 = timedelta(seconds=60)
-		c1 = c2 = c3 = 0
-		num = 0
-		for i in range(len(order_time)-1,0,-1):
-			num += ord_values[i]
-			if order_time[i]+time1<deal_time and c1==0:
-				c1=num
-			if order_time[i]+time2<deal_time and c2==0:
-				c2=num
-			if order_time[i]+time3<deal_time and c3==0:
-				c3=num
-				break
-		# newdata = [time.mktime(deal_time.timetuple()),price,val,c1,c2,c3]
-		newdata = [c1,c2,c3]
-		# print time.mktime(deal_time.timetuple())
-		if deals == None:
-			deals = newdata
-		else:
-			deals = np.vstack((deals, newdata))
-
-		data_price.append(price)
-		
+def Extremes(data_price):
 	minpoints = []
 	maxpoints = []
 	lastmax = lastmin = 1
@@ -110,31 +52,105 @@ def main():
 					lastmin = data_price[i]
 					lastIsMax = False
 
+	return maxpoints,minpoints
+
+def main():
+
+	df = open(DEAL,"r")
+	of = open(ORDER,"r")
+
+	order_time = []
+	ord_values = []
+	deals = None
+	line_num=0
+	color = np.zeros(N)
+	data_price =[]
+	data_time = []
+	for line in df:
+		line_num+=1
+		if line_num> N:
+			break
+		if line[0] == '#':
+			continue
+		date = line.split(',')[2]
+		price = int(line.split(',')[4].split('.')[0])
+		val = int(line.split(',')[5])
+		deal_type = line.split(',')[7]
+		# print date, "\t", price, "\t", val, "\t", deal_type,
+		deal_time = datetime.strptime(date, "%Y%m%d%H%M%S%f")
+		while True:
+			ordline = of.readline()
+			if ordline[0] == '#':
+				continue
+			ord_time = datetime.strptime(ordline.split(',')[3], "%Y%m%d%H%M%S%f")
+			order_time.append(ord_time)
+			ord_values.append(int(ordline.split(',')[7]))
+			if(ord_time>deal_time):
+				break
+
+		# time1 = datetime.timedelta([days[, seconds[, microseconds[, milliseconds[, minutes[, hours[, weeks]]]]]]])
+		time1 = timedelta(microseconds=100)
+		time2 = timedelta(seconds=1)
+		time3 = timedelta(seconds=2)
+		time4 = timedelta(seconds=5)
+		c1 = c2 = c3 = c4 = 0
+		v1 = v2 = v3 = v4 = 0
+		num = 0
+		for i in range(len(order_time)-1,0,-1):
+			num += ord_values[i]
+			if order_time[i]+time1<deal_time and c1==0:	c1=num
+			if order_time[i]+time2<deal_time and c2==0: c2=num
+			if order_time[i]+time3<deal_time and c3==0: c3=num
+			if order_time[i]+time4<deal_time and c4==0:	
+				c4=num
+				break
+
+		for i in range(len(data_price)-1,0,-1):
+			if data_time[i]+time1<deal_time and v1==0:	v1 = price - data_price[i]
+			if data_time[i]+time2<deal_time and v2==0:	v2 = price - data_price[i]
+			if data_time[i]+time3<deal_time and v3==0:	v3 = price - data_price[i]
+			if data_time[i]+time4<deal_time and v4==0:	
+				v4 = price - data_price[i]
+				break
+		# newdata = [time.mktime(deal_time.timetuple()),price,val,c1,c2,c3]
+		newdata = [c1,c2,c3,c4,v1,v2,v3,v4]
+		# print time.mktime(deal_time.timetuple())
+		if deals == None:
+			deals = newdata
+		else:
+			deals = np.vstack((deals, newdata))
+
+		data_price.append(price)
+		data_time.append(deal_time)
+		
+	maxpoints,minpoints = Extremes(data_price)
+
 	# print maxpoints
 	# print minpoints
 
+
 	for i in maxpoints:
-		color[i] = 2
+		color[i] = 3
 		j=i
+		time1 = timedelta(microseconds=100)
+		while order_time[j]+time1>order_time[i] and j>0:
+			j-=1
+			color[j] = 2
 		time1 = timedelta(seconds=1)
 		while order_time[j]+time1>order_time[i] and j>0:
 			j-=1
-			color[j] = 3
-		time1 = timedelta(seconds=5)
-		# while order_time[j]+time1>order_time[i] and j>0:
-		# 	j-=1
-		# 	color[j] = 4
+			color[j] = 1
 	for i in minpoints:
-		color[i] = 5
+		color[i] = -3
 		j=i
+		time1 = timedelta(microseconds=100)
+		while order_time[j]+time1>order_time[i] and j>0:
+			j-=1
+			color[j] = -2
 		time1 = timedelta(seconds=1)
 		while order_time[j]+time1>order_time[i] and j>0:
 			j-=1
-			color[j] = 6
-		time1 = timedelta(seconds=5)
-		# while order_time[j]+time1>order_time[i] and j>0:
-		# 	j-=1
-		# 	color[j] = 7
+			color[j] = -1
 
 	A = pca(deals)
 	i = 0
